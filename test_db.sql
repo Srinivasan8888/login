6bdd2ae89b3feaaaab3a20de605157db21cb0ce3
Srinivasan8888 <92676627+Srinivasan8888@users.noreply.github.com>
2022-10-19 14:18:09 +0530

    added changed in db

--- test_db.sql
+++ test_db.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 16, 2020 at 10:22 AM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Host: localhost
+-- Generation Time: Oct 19, 2022 at 10:46 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,6 +20,45 @@ SET time_zone = "+00:00";
 --
 -- Database: `test_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `S_No` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`S_No`, `email`, `password`) VALUES
+(1, 'rGs/LKD7fT7zM/F4sHZmrA==', 'NTX+934GT6DBSv4qr4BIUg==');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Employee`
+--
+
+CREATE TABLE `Employee` (
+  `Name` varchar(255) NOT NULL,
+  `Phone_No` int(12) NOT NULL,
+  `Id` int(11) NOT NULL,
+  `City` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `Employee`
+--
+
+INSERT INTO `Employee` (`Name`, `Phone_No`, `Id`, `City`) VALUES
+('Srini', 770823412, 1, 'mdu');
 
 -- --------------------------------------------------------
 
@@ -38,12 +76,18 @@ CREATE TABLE `images` (
 --
 
 INSERT INTO `images` (`id`, `image_url`) VALUES
-(8, 'IMG-5f8954bd209a92.78214246.jpg'),
-(9, 'IMG-5f8954caa02539.76436861.jpg');
+(17, 'IMG-634f9a0f3530b3.76135964.png'),
+(18, 'IMG-634f9c79e1ccf7.06112873.png');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `Employee`
+--
+ALTER TABLE `Employee`
+  ADD PRIMARY KEY (`Id`);
 
 --
 -- Indexes for table `images`
@@ -56,10 +100,16 @@ ALTER TABLE `images`
 --
 
 --
+-- AUTO_INCREMENT for table `Employee`
+--
+ALTER TABLE `Employee`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
